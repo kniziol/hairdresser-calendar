3c473e55206d6a2c5d22ae40bc78a4d9fd5acf4d
Krzysztof Nizioł <krzysztof.niziol@meritoo.pl>
2019-08-28 22:42:32 +0200

    Install PHPUnit

---
+++ tests/DummyTest.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @internal
+ */
+class DummyTest extends TestCase
+{
+    public function testDummy(): void
+    {
+        static::assertSame(1, 1);
+    }
+}
